64e91584f94314bda08e38a326749a383f176bbd
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-19 14:15:53 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker-compose up -d
-docker exec -d astrobee_container $HOME/freeflyer_build/native $$ source devel/setup.bash && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true
+docker exec -d astrobee_sim_container $HOME/freeflyer_build/native $$ source devel/setup.bash && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true
