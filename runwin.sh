cda7b438d5514d666d695bb4484ea7f3d5b90439
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-20 09:28:27 -0700

    Update runwin.sh
    missed the rviz:=true flag

--- runwin.sh
+++ runwin.sh
@@ -4,7 +4,7 @@ docker container stop $(docker container ls -aq)
 
 docker-compose up -d
 winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
-&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub"
+&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
 echo "Standby for browser interface..."
 sleep 3
 echo "Opening browser interface"
