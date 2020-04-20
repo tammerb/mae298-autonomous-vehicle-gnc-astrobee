67236d29fb38abbfad5be0d189eb0a94ada2bea7
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-20 09:27:00 -0700

    Update runwin.sh
    missed the "source /root..."  command when creating this

--- runwin.sh
+++ runwin.sh
@@ -3,7 +3,8 @@
 docker container stop $(docker container ls -aq)
 
 docker-compose up -d
-winpty docker exec -d astrobee_sim_container //bin//bash -c > && roslaunch astrobee sim.launch dds:=false robot:=sim_pub
+winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
+&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub"
 echo "Standby for browser interface..."
 sleep 3
 echo "Opening browser interface"
