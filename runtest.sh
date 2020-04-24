52f0cbea35da2d40a569cd490f85d28ebe23f25d
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-24 14:03:40 -0700

    Update runtest.sh
    add OR winpty for the docker exec command

--- runtest.sh
+++ runtest.sh
@@ -4,7 +4,8 @@ docker container stop $(docker container ls -aq)
 
 docker-compose up -d
 docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build/native/devel/setup.bash \
+&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
 && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
 echo "Opening browser interface..."
-sleep 1
+sleep 3
 xdg-open http://localhost:8080/vnc_auto.html & || open http://localhost:8080/vnc_auto.html &
