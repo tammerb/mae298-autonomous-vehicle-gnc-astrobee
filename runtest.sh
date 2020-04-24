4ef81bfa388f3f789f6b9ee76e4630df72606d19
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-24 14:10:42 -0700

    Update runtest.sh
    change curly braces to quotes

--- runtest.sh
+++ runtest.sh
@@ -8,4 +8,4 @@ docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build
 && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
 echo "Opening browser interface..."
 sleep 3
-{xdg-open http://localhost:8080/vnc_auto.html &} || {open http://localhost:8080/vnc_auto.html &} || {start http://localhost:8080/vnc_auto.html &}
+xdg-open "http://localhost:8080/vnc_auto.html &" || open "http://localhost:8080/vnc_auto.html &" || start "http://localhost:8080/vnc_auto.html &"
