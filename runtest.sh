5c8bc133d2cea55473dce701395e096e95b3d85a
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-25 12:25:57 -0700

    Update runtest.sh
    add -cam flag to turn on dock and nav cams at 1Hz

--- runtest.sh
+++ runtest.sh
@@ -3,11 +3,21 @@
 docker container stop $(docker container ls -aq)
 
 docker-compose up -d
-docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build/native/devel/setup.bash \
-&& sed -i 's/nav_cam_rate = 0.0/nav_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config \
-&& sed -i 's/dock_cam_rate = 0.0/doc_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config \
-&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
+docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build/native/devel/setup.bash"
+
+for arg in "$@"
+do
+  case $arg in
+    -cam)
+    docker exec -d astrobee_sim_container /bin/bash -c "sed -i 's/nav_cam_rate = 0.0/nav_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config \
+      && sed -i 's/dock_cam_rate = 0.0/doc_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config"
+  esac
+done
+
+
+docker exec -d astrobee_sim_container /bin/bash -c "roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
 && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
+
 echo "Opening browser interface..."
 sleep 3
 xdg-open http://localhost:8080/vnc_auto.html &
