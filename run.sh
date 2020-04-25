dcb6fd69801c3059c81c98a1d6cc2d0b556376c3
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-25 11:50:11 -0700

    Update run.sh
    Remove the OR commands, add error supression

--- run.sh
+++ run.sh
@@ -3,11 +3,15 @@
 docker container stop $(docker container ls -aq)
 
 docker-compose up -d
+
 docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build/native/devel/setup.bash \
 && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
 && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
+
 echo "Standby. Opening browser interface..."
+
 sleep 3
-xdg-open http://localhost:8080/vnc_auto.html & \
-|| open http://localhost:8080/vnc_auto.html & \
-|| start http://localhost:8080/vnc_auto.html &
+
+xdg-open http://localhost:8080/vnc_auto.html & 2>/dev/null
+open http://localhost:8080/vnc_auto.html & 2>/dev/null
+start http://localhost:8080/vnc_auto.html & 2>/dev/null
