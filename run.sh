1494485775a45f78c091341e3a6d50b1bc539c54
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-25 11:25:38 -0700

    Update run.sh
    Move OS-agnostic run commands to this script.

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ docker container stop $(docker container ls -aq)
 
 docker-compose up -d
 docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build/native/devel/setup.bash \
+&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
 && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
-echo "Opening browser interface..."
-sleep 1
-xdg-open http://localhost:8080/vnc_auto.html & || open http://localhost:8080/vnc_auto.html &
+echo "Standby. Opening browser interface..."
+sleep 3
+xdg-open http://localhost:8080/vnc_auto.html &
+open http://localhost:8080/vnc_auto.html &
+start http://localhost:8080/vnc_auto.html &
