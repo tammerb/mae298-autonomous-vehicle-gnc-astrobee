7b66ac6c2fffbf688dd5c805789dd53dea0f570e
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-25 22:52:44 -0700

    Update run.sh
    Move quotation marks around to just the source commands.

--- run.sh
+++ run.sh
@@ -4,19 +4,13 @@ docker container stop $(docker container ls -aq)
 
 docker-compose up -d
 
-sleep 1
+docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build/native/devel/setup.bash" \
+&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash" \
+&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true
 
-docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build/native/devel/setup.bash \
-&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
-&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
+echo "Standby." Opening browser interface. This may throw some 'command not found errors'..."
 
-echo "Standby. Opening browser interface..."
-
-sleep 1
-
-echo "If browser opens, ignore the following 'command not found' errors"
-
-sleep 1
+sleep 2
 
 xdg-open http://localhost:8080/vnc_auto.html &
 open http://localhost:8080/vnc_auto.html &
