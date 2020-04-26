7f8f8f13d457d874bce0de6d875dfd8c2b2914b1
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-25 23:38:05 -0700

    Update runcams.sh
    use variables for paths to config and setup

--- runcams.sh
+++ runcams.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+CAM_CONFIG=/root/myfreeflyer/astrobee/config/simulation/simulation.config
+SETUP=/root/freeflyer_build/native/devel/setup.bash
 
 docker container stop $(docker container ls -aq)
 
 docker-compose up -d
-docker exec -d astrobee_sim_container /bin/bash -c "source /root/freeflyer_build/native/devel/setup.bash \
-&& sed -i 's/dock_cam_rate = 0.0/dock_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config \
-&& sed -i 's/nav_cam_rate = 0.0/nav_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config \
-&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash \
+docker exec -d astrobee_sim_container /bin/bash -c "source $SETUP \
+&& sed -i 's/dock_cam_rate = 0.0/dock_cam_rate = 1.0/' $CAM_CONFIG \
+&& sed -i 's/nav_cam_rate = 0.0/nav_cam_rate = 1.0/' $CAM_CONFIG \
+&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" \
+|| winpty docker exec -d astrobee_sim_container //bin//bash -c "source SETUP \
 && roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
+
 echo "Opening browser interface..."
+
 sleep 3
+
 xdg-open http://localhost:8080/vnc_auto.html &
 open http://localhost:8080/vnc_auto.html &
 start http://localhost:8080/vnc_auto.html &
