2a678e9c80504d94e18d6c0a28b8f80cb0335c5f
tammerb <32876279+tammerb@users.noreply.github.com>
2020-04-25 22:59:59 -0700

    Update runtest.sh
    Move quotations around

--- runtest.sh
+++ runtest.sh
@@ -9,14 +9,16 @@ for arg in "$@"
 do
   case $arg in
     -cam)
-    docker exec -d astrobee_sim_container /bin/bash -c "sed -i 's/nav_cam_rate = 0.0/nav_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config \
+    docker exec -d astrobee_sim_container /bin/bash -c \
+    "sed -i 's/nav_cam_rate = 0.0/nav_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config \
       && sed -i 's/dock_cam_rate = 0.0/doc_cam_rate = 1.0/' /root/myfreeflyer/astrobee/config/simulation/simulation.config"
   esac
 done
 
 
-docker exec -d astrobee_sim_container /bin/bash -c "roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" || winpty docker exec -d astrobee_sim_container //bin//bash -c "source /root/freeflyer_build/native/devel/setup.bash" \
-&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true
+docker exec -d astrobee_sim_container /bin/bash -c "roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true" \
+|| winpty docker exec -d astrobee_sim_container //bin//bash -c \
+"source /root/freeflyer_build/native/devel/setup.bash&& roslaunch astrobee sim.launch dds:=false robot:=sim_pub rviz:=true"
 
 echo "Opening browser interface..."
 sleep 3
