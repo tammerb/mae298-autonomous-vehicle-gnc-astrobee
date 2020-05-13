30ff890fa72f54b021efc44aa1e6f8b077d4df42
tammerb <32876279+tammerb@users.noreply.github.com>
2020-05-12 23:14:46 -0700

    Create readbag.py

---
+++ bag/readbag.py
@@ -0,0 +1,8 @@
+import rosbag
+bag = rosbag.Bag('ar_features.bag')
+topics = bag.get_type_and_topic_info()[1].keys()
+types = []
+for i in range(0,len(bag.get_type_and_topic_info()[1].values())):
+        types.append(bag.get_type_and_topic_info()[1].values()[i][0])
+for type in types:
+        print(type)
